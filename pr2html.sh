5bbd00c783ec0c6e080323e73c9900c034980107
Paresh Adhia <paresh_adhia@yahoo.com>
2019-09-26 07:40:55 -0400

    WIP

--- pr2html.sh
+++ pr2html.sh
@@ -1,3 +1,21 @@
 #! /bin/sh
 
-pandoc "${1:?filen name?}" -c "$HOME/raas-garba/pr2html.css" -s -o /tmp/"${1%.*}.html"
+pandoc() {
+    docker run -i \
+        --rm \
+        -h pandoc \
+        --mount type=bind,source=$HOME,target=$HOME \
+        --mount type=bind,source=/tmp,target=/tmp \
+        pandoc/core "$@"
+}
+
+HERE="$(dirname $(realpath $0))"
+
+for F; do
+    INPUT="$(realpath "$F")"
+    OUTPUT="$(basename "$INPUT")"
+    OUTPUT="/tmp/"${OUTPUT%.*}.html
+
+    echo "$OUTPUT"
+    pandoc "$INPUT" -c "$HERE/pr2html.css" -s -o "$OUTPUT"
+done
