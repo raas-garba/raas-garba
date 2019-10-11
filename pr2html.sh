04a7967bb0536ba75f360f9c3c4ab01d0c9f22b9
Paresh Adhia <paresh_adhia@yahoo.com>
2019-10-10 21:11:01 -0400

    WIP

--- pr2html.sh
+++ pr2html.sh
@@ -1,14 +1,5 @@
 #! /bin/sh
 
-pandoc() {
-    docker run -i \
-        --rm \
-        -h pandoc \
-        --mount type=bind,source=$HOME,target=$HOME \
-        --mount type=bind,source=/tmp,target=/tmp \
-        pandoc/core "$@"
-}
-
 HERE="$(dirname $(realpath $0))"
 
 for F; do
